e58adfd9276327dc9598e07c4f33f8997e9adbd4
Merey <146691446+merfihoney@users.noreply.github.com>
2024-02-06 19:17:49 +0600

    Add files via upload

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func connectDB() (*sql.DB, error) {
+	db, err := sql.Open("postgres", "user=postgres password=6465 dbname=Users host=localhost port=5432 sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func main() {
+	connStr := "user=postgres dbname=Users password=6465 sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully connected to the database")
+	defer db.Close()
+
+}
+
+func createTable(db *sql.DB) {
+	// Create the "people" table if it doesn't exist
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS people (
+			id SERIAL PRIMARY KEY,
+			name VARCHAR(255),
+			age INTEGER
+		)
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
